572f348228ac76b1298a2eb247a5acf244553093
Lynn <htklassy@gmail.com>
2022-04-14 13:24:05 -0600

    Setting up project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-Header"></header>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
